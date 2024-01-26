cfcbc8a830ed9116d5bf2c4e1019774bb44fa413
Andrey <152691772+cycler11@users.noreply.github.com>
2024-01-27 00:02:26 +0300

    Delete bf.py

--- bf.py
+++
@@ -1,9 +0,0 @@
-#import this function to use
-#example: from bf read_passwords
-
-def read_passwords(filename):
-    with open(filename, 'r') as file:
-        for line in file:
-            yield line.strip()
-
-
